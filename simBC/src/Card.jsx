a184fa2ffbb665bf5d8dc758177c2bff40ce4994
SW33TSTUFF <simaakniyaz@gmail.com>
2024-07-26 16:29:52 +0530

    React Cards

---
+++ simBC/src/Card.jsx
@@ -0,0 +1,13 @@
+
+
+function Card() {
+    return (
+        <div className="card">
+            <img className="card-image" src="https://picsum.photos/seed/picsum/200/300" alt="profile picture"></img>
+            <h2 className="card-title">SIMAAK</h2>
+            <p className="card-text">I make YouTube videos and play video games</p>
+        </div>
+    );
+}
+
+export default Card;
